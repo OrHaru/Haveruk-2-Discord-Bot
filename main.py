8eaf4efd588530a8dc387f5da6d9c983dd1ee941
OrHaru <orharush555@gmail.com>
2023-03-12 01:46:29 +0200

    new buttons

--- main.py
+++ main.py
@@ -26,11 +26,9 @@ async def on_voice_state_update(member, before, after):
     # Move the member to the new voice channel and set permissions
     await member.move_to(voice_channel)
     await voice_channel.set_permissions(member,
-                                        manage_channels=True,
-                                        manage_roles=True)
+                                        connect = True, view_channel = True)
     await text_channel.set_permissions(member,
-                                       manage_channels=True,
-                                       manage_roles=True)
+                                       view_channel = True)
 
     # Add buttons for making channel public and private
     unlock_button = Button(style=discord.ButtonStyle.green,
@@ -39,11 +37,21 @@ async def on_voice_state_update(member, before, after):
     lock_button = Button(style=discord.ButtonStyle.red,
                          label="Lock",
                          custom_id="lock")
+    visible_button = Button(style=discord.ButtonStyle.red,
+                         label="Visible",
+                         custom_id="visible")
+    invisible_button = Button(style=discord.ButtonStyle.red,
+                         label="Invisible",
+                         custom_id="invisible")
     view = View()
     view.add_item(unlock_button)
     view.add_item(lock_button)
+    view.add_item(visible_button)
+    view.add_item(invisible_button)
     unlock_button.callback = button_unlock
     lock_button.callback = button_lock
+    visible_button.callback = button_visible
+    invisible_button.callback = button_invisible
 
     message = f"Welcome to your new channel, {member.mention}!"
     message = await text_channel.send(message, view=view)
@@ -75,9 +83,7 @@ async def button_lock(interaction):
         interaction.guild.default_role:
         discord.PermissionOverwrite(connect=False),
         interaction.user:
-        discord.PermissionOverwrite(connect=True,
-                                    manage_channels=True,
-                                    manage_permissions=True),
+        discord.PermissionOverwrite(connect = True, view_channel = True),
         bot.user:
         discord.PermissionOverwrite(connect=True)
       }
@@ -99,9 +105,7 @@ async def button_unlock(interaction):
         interaction.guild.default_role:
         discord.PermissionOverwrite(connect=True),
         interaction.user:
-        discord.PermissionOverwrite(connect=True,
-                                    manage_channels=True,
-                                    manage_permissions=True),
+        discord.PermissionOverwrite(connect = True, view_channel = True),
         bot.user:
         discord.PermissionOverwrite(connect=True)
       }
@@ -112,6 +116,49 @@ async def button_unlock(interaction):
       await interaction.response.send_message(
         "You're not in your voice channel!", ephemeral=True)
 
+
+@bot.command()
+async def button_invisible(interaction):
+  if interaction.user.voice:
+    channel = interaction.user.voice.channel
+    if interaction.user.id == created_channels[channel.id]["member_id"]:
+
+      overwrites = {
+        interaction.guild.default_role:
+        discord.PermissionOverwrite(view_channel = False),
+        interaction.user:
+        discord.PermissionOverwrite(connect = True, view_channel = True),
+        bot.user:
+        discord.PermissionOverwrite(connect=True)
+      }
+      await channel.edit(overwrites=overwrites)
+      await interaction.response.send_message("This channel is now invisible!",
+                                              ephemeral=True)
+    else:
+      await interaction.response.send_message(
+        "You're not in your voice channel!", ephemeral=True)
+
+
+@bot.command()
+async def button_visible(interaction):
+  if interaction.user.voice:
+    channel = interaction.user.voice.channel
+    if interaction.user.id == created_channels[channel.id]["member_id"]:
+
+      overwrites = {
+        interaction.guild.default_role:
+        discord.PermissionOverwrite(view_channel = True),
+        interaction.user:
+        discord.PermissionOverwrite(connect = True, view_channel = True),
+        bot.user:
+        discord.PermissionOverwrite(connect=True)
+      }
+      await channel.edit(overwrites=overwrites)
+      await interaction.response.send_message("This channel is now visible!",
+                                              ephemeral=True)
+    else:
+      await interaction.response.send_message(
+        "You're not in your voice channel!", ephemeral=True)
 
 keep_alive()
 bot.run(DISCORD_TOKEN)
